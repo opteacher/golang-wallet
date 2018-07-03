0096a34b314b14e748d1388f769620a90e7a8e26
opteacher <george.zhao@viewfin.com>
2018-07-03 23:39:16 +0800

    Test redis

--- build.bat
+++ build.bat
@@ -3,6 +3,6 @@ set /p input=Whether install/update modules(Y/n):
 if %input%==Y (
 	go get -u github.com/go-sql-driver/mysql
 	go get -u github.com/stretchr/testify
-	go get -u github.com/jinzhu/gorm
+	go get -u github.com/go-redis/redis
 )
 go build wallet.go
--- tests/redis_test.go
+++ tests/redis_test.go
@@ -2,21 +2,34 @@ package tests
 
 import (
 	"testing"
-	"databases"
 	"github.com/go-redis/redis"
+	"utils"
 	"log"
-	"fmt"
+	"time"
 )
 
 func TestRedis(t *testing.T) {
-	var cli *redis.Cmdable
-	var err error
-	if cli, err = databases.ConnectRedis(); err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println(cli)
-
+	//var cli *redis.Cmdable
+	//var err error
+	//if cli, err = databases.ConnectRedis(); err != nil {
+	//	log.Fatal(err)
+	//}
+	//
 	//if err = (*cli).Set("abcd", "helloWorld", 20 * time.Second).Err(); err != nil {
 	//	log.Fatal(err)
 	//}
+
+	redisCfg := utils.GetConfig().GetSubsSettings().Redis
+	redisCli := redis.NewClient(&redis.Options {
+		Addr: redisCfg.Clusters[0].Url,
+		Password: redisCfg.Password,
+		DB: 0,
+	})
+	err := redisCli.Ping().Err()
+	if err != nil {
+		log.Fatal(err)
+	}
+	if err = redisCli.Set("abcd", "helloWorld", 20 * time.Second).Err(); err != nil {
+		log.Fatal(err)
+	}
 }
