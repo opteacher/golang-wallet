d7e021cc72fcbbcd66452731257a2d4b5098a17b
opower <george.zhao@viewfin.com>
2018-08-10 11:04:41 +0800

    Changed some setting of prod

--- sql/database.sql
+++ sql/database.sql
@@ -1,2 +1,2 @@
 # CreateDatabase
-CREATE DATABASE IF NOT EXISTS `test` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+CREATE DATABASE IF NOT EXISTS `wallet_service` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
